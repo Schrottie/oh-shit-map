72f5ea542cafff9cdbd02048f61f7053ca062c3f
Maik Bischoff <schrottie@gmail.com>
2023-11-20 06:10:50 +0100

    Update map.php

--- map.php
+++ map.php
@@ -6,41 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oh-Shit-Map</title>
 
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-    <style>
-        #map-container {
-            position: relative;
-        }
-
-        #map {
-            height: 98vh;
-            z-index: 1;
-        }
-
-        #map-title {
-            position: absolute;
-            top: 10px;
-            left: 50%;
-            transform: translateX(-70%);
-            padding: 5px 15px 14px 15px;
-            margin-bottom: 0;
-            background-color: rgba(255, 255, 255, 0.9); 
-            border-radius: 15px;
-            border: solid 1px black;
-            font-family: 'Lexend Deca', sans-serif; 
-            font-size: 1.8em;
-            color: black;
-            z-index: 2;
-			line-height: 0.4;
-            text-align: center;
-        }
-        
-        #map-title .sub {
-			font-size: 0.43em;
-			margin-top: 0px;
-			padding-top: 0px;
-		}
-    </style>
+    <!-- Leaflet Styles -->
+    <link rel="css/leaflet.css" />
+    <!-- Eigene Styles -->
+    <link rel="css/map.css" />
     <!-- Google Fonts Lexend Deca -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@400&display=swap">
 
@@ -55,7 +24,7 @@
 			<br /><span class="sub">proudly presented by: Schrotties Laufblog "<a href="https://running.maik-bischoff.de/" target="foo">100 Meilen</a>"</span>
 		</div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script src="js/leaflet.js"></script>
 
     <script>
         var map = L.map('map').setView([52.5200, 13.4050], 13);

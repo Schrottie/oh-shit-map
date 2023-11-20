1de8d023dfb11e83fd5c23650a29ff4b5a6f136b
Maik Bischoff <schrottie@gmail.com>
2023-11-20 09:30:29 +0100

    Create geolocation.js

---
+++ js/geolocation.js
@@ -0,0 +1,17 @@
+// geolocation.js
+
+function zentriereAufStandort(map) {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                const latlng = [position.coords.latitude, position.coords.longitude];
+                map.setView(latlng, 13);
+            },
+            function (error) {
+                console.error('Fehler bei der Standortermittlung:', error.message);
+            }
+        );
+    } else {
+        console.error('Geolocation wird nicht unterstÃ¼tzt.');
+    }
+}

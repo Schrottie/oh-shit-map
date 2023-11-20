e7a8b66d44962f3d2d05909b253d3d1641ff8cf8
Maik Bischoff <schrottie@gmail.com>
2023-11-20 13:01:33 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@
 var map = L.map('map').setView([52.5200, 13.4050], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+    attribution: '© Map tiles by <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>, under <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank">CC BY 3.0</a>. Data by <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>, under <a href="https://opendatacommons.org/licenses/odbl/" target="_blank">ODbL</a>.'
 }).addTo(map);
 
 var toilettenLayer = L.layerGroup().addTo(map);

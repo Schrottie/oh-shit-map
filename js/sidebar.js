69b862b81825bcf7ee8714ab6ada9ca5bfbbfeca
Maik Bischoff <schrottie@gmail.com>
2023-11-26 11:28:53 +0100

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     var sidebar = document.getElementById('sidebar');
     var sidebarToggle = document.getElementById('sidebar-toggle');
+    var download = document.getElementById('download-button');
 
     sidebarToggle.addEventListener('click', function () {
         sidebar.classList.toggle('show');
         sidebarToggle.classList.toggle('show');
+        download.classList.toggle('show');
     });
 });

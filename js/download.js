d4ef0f86581a739e64308f1a3aa735c3c49c5abe
Bischoff <maik.bischoff@berliner-feuerwehr.de>
2023-11-29 11:51:39 +0100

    Let Chat-GPT write comments for all the JS stuff. ;)

--- js/download.js
+++ js/download.js
@@ -1,40 +1,55 @@
+// Warte darauf, dass das DOM vollständig geladen ist, bevor das Skript ausgeführt wird
 document.addEventListener('DOMContentLoaded', function () {
+    // Füge einen Event-Listener für den Download-Button hinzu, wenn das DOM geladen ist
     addDownloadLinkListener();
 });
 
+// Funktion zum Hinzufügen des Event-Listeners für den Download-Button
 function addDownloadLinkListener() {
+    // Finde das DOM-Element des Download-Buttons anhand der ID
     const downloadLink = document.getElementById('download-button');
 
+    // Wenn der Download-Button gefunden wurde, füge einen Event-Listener hinzu
     if (downloadLink) {
         downloadLink.addEventListener('click', downloadToilettenGPX);
     }
 }
 
+// Funktion zum Herunterladen von Toiletten-Daten im GPX-Format
 function downloadToilettenGPX() {
+    // Ermittle die Begrenzungen der aktuellen Kartenansicht
     var bounds = map.getBounds();
+    // Baue eine Overpass-Abfrage basierend auf den Kartenbegrenzungen
     var overpassQuery = buildOverpassQuery(bounds);
 
+    // Führe einen POST-Request zur Overpass-API durch, um Toiletten-Daten abzurufen
     fetch('https://overpass-api.de/api/interpreter', {
         method: 'POST',
         body: overpassQuery
     })
     .then(response => response.json())
     .then(data => {
+        // Wenn Daten vorhanden sind, erstelle eine GPX-Datei und lade sie herunter
         if (data.elements) {
             const gpxData = createGPX(data.elements);
-
             downloadFile(gpxData, 'toiletten.gpx');
         } else {
+            // Wenn keine Toiletten-Daten gefunden wurden, gib eine Fehlermeldung aus
             console.error('Keine Toiletten-Daten gefunden.');
         }
     })
     .catch(error => {
+        // Behandele Fehler beim Abrufen der Toiletten-Daten
         console.error('Fehler beim Abrufen der Toiletten-Daten:', error);
     });
 }
+
+// Funktion zum Erstellen von GPX-Daten aus den abgerufenen Toiletten-Elementen
 function createGPX(elements) {
+    // Initialisiere einen GPXBuilder
     const gpxBuilder = new GPXBuilder();
 
+    // Iteriere über die Toiletten-Elemente und füge GPX-Punkte hinzu
     elements.forEach(toilet => {
         const latlng = [toilet.lat, toilet.lon];
 
@@ -50,30 +65,39 @@ function createGPX(elements) {
         gpxBuilder.addPoint(gpxPoint);
     });
 
+    // Erhalte die vollständigen GPX-Daten
     const gpxData = gpxBuilder.toString();
-    downloadGPXFile(gpxData, 'toiletten.gpx');
+    return gpxData;
 }
 
-function downloadGPXFile(data, filename) {
+// Funktion zum Herunterladen von GPX-Daten als Datei
+function downloadFile(data, filename) {
+    // Erstelle eine Blob mit den GPX-Daten
     const blob = new Blob([data], { type: 'application/gpx+xml' });
+    // Erstelle ein Link-Element für den Download
     const link = document.createElement('a');
+    // Setze die URL des Links auf die GPX-Daten
     link.href = window.URL.createObjectURL(blob);
+    // Setze den Dateinamen für den Download
     link.download = filename;
 
+    // Füge den Link zum DOM hinzu, triggere den Klick-Event und entferne den Link wieder
     document.body.appendChild(link);
     link.click();
-
     document.body.removeChild(link);
 }
 
+// Klasse für den Aufbau von GPX-Daten
 class GPXBuilder {
     constructor() {
+        // Initialisiere die GPX-Daten mit dem Header
         this.gpxData = '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>\n' +
             '<gpx version="1.1" creator="YourAppName">\n' +
             '<trk>\n' +
             '<trkseg>\n';
     }
 
+    // Füge einen GPX-Punkt hinzu
     addPoint(point) {
         this.gpxData += `<trkpt lat="${point.lat}" lon="${point.lon}">\n`;
         this.gpxData += `<ele>${point.ele}</ele>\n`;
@@ -83,6 +107,7 @@ class GPXBuilder {
         this.gpxData += '</trkpt>\n';
     }
 
+    // Schließe die GPX-Daten ab
     toString() {
         this.gpxData += '</trkseg>\n' +
             '</trk>\n' +
--- js/overpass.js
+++ js/overpass.js
@@ -1,24 +1,34 @@
+// Funktion zum Laden und Anzeigen von Toiletten-Daten auf der Karte
 function loadToilettenLayer(map, toilettenLayer) {
+    // Ermittle die Begrenzungen der aktuellen Kartenansicht
     var bounds = map.getBounds();
+    // Baue eine Overpass-Abfrage basierend auf den Kartenbegrenzungen
     var overpassQuery = buildOverpassQuery(bounds);
 
+    // Führe einen POST-Request zur Overpass-API durch, um Toiletten-Daten abzurufen
     fetch('https://overpass-api.de/api/interpreter', {
         method: 'POST',
         body: overpassQuery
     })
     .then(response => response.json())
     .then(data => {
+        // Überprüfe, ob Daten vorhanden sind
         if (data.elements) {
+            // Lösche bestehende Marker in der Toiletten-LayerGroup
             toilettenLayer.clearLayers();
 
+            // Iteriere über die abgerufenen Toiletten-Daten und füge Marker hinzu
             data.elements.forEach(toilet => {
                 const latlng = [toilet.lat, toilet.lon];
 
+                // Wähle das passende Icon für die Toilette basierend auf den Tags
                 const iconUrl = toilet.tags['toilets:position'] === 'urinal'
                     ? 'img/pipi.png'
                     : 'img/klo.png';
 
+                // Überprüfe, ob der Zugang zur Toilette erlaubt ist
                 if (toilet.tags.access && toilet.tags.access.toLowerCase() === 'permissive') {
+                    // Erstelle einen Marker mit "verbotenem" Icon und Popup-Inhalt
                     const forbiddenIcon = L.icon({
                         iconUrl: 'img/no-klo.png',
                         iconSize: [32, 32],
@@ -37,6 +47,7 @@ function loadToilettenLayer(map, toilettenLayer) {
 
                     toilettenLayer.addLayer(marker);
                 } else {
+                    // Erstelle einen Marker mit normalem Toiletten-Icon und Popup-Inhalt
                     const toiletIcon = L.icon({
                         iconUrl: iconUrl,
                         iconSize: [32, 32],
@@ -60,9 +71,10 @@ function loadToilettenLayer(map, toilettenLayer) {
     });
 }
 
-
+// Funktion zum Formatieren von Tags für die Anzeige im Popup
 function formatTags(tags) {
     const formattedTags = Object.entries(tags).map(([key, value]) => {
+        // Wenn es sich um die Website handelt, erstelle einen Link
         if (key === 'website') {
             return `${key}: <a href="${value}" target="_blank">${value}</a>`;
         } else {
@@ -73,6 +85,7 @@ function formatTags(tags) {
     return formattedTags.join('<br>');
 }
 
+// Funktion zum Erstellen einer Overpass-Abfrage basierend auf den Kartenbegrenzungen
 function buildOverpassQuery(bounds) {
     var query = '[out:json];' +
         '(' +
